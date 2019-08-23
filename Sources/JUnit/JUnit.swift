140a4258bd7d7320e548f852bbb9ed81f713d0cc
Simon-Pierre Roy <sroy1@wayfair.com>
2019-08-23 08:21:51 -0400

    changed API

--- Sources/JUnit/JUnit.swift
+++ Sources/JUnit/JUnit.swift
@@ -13,9 +13,9 @@ import Prelude
 public enum JUnit {
     
     public struct Tagged<Tag, RawValue> {
-        public let rawValue: RawValue
+        private let rawValue: RawValue
         
-        fileprivate init(_ rawValue: RawValue) {
+        private init(_ rawValue: RawValue) {
             self.rawValue = rawValue
         }
     }
@@ -39,138 +39,65 @@ public enum JUnit {
     public typealias ElementNode = Refined<XMLElement, IsXMLElement>
     public typealias AttributeNode = Refined<XMLNode, IsXMLAttribute>
     public typealias Document = Tagged<JUnit, XMLDocument>
-  
+    
     public enum Failure {
-        
         public typealias Element = Tagged<Failure, ElementNode>
         public typealias Attribute = Tagged<Failure, AttributeNode>
-        
-        public static func new(withText text: String) -> Element {
-            return .init(.new(withName: "failure", text: text))
-        }
-        
-        public static func message(text: String) -> Attribute {
-            return .init(.new(withName: "message", text: text))
-        }
-        
-        public static func type(text: String) -> Attribute {
-            return .init(.new(withName: "type", text: text))
-        }
     }
     
     public enum TestCase {
-        
         public typealias Element = Tagged<TestCase, ElementNode>
         public typealias Attribute = Tagged<TestCase, AttributeNode>
-        
-        public static func new() -> Element {
-            return .init(.new(withName: "testcase", text: nil))
-        }
-        
-        public static func id(value: UUID) -> Attribute {
-            return .init(.new(withName: "id", text: value.uuidString))
-        }
-        
-        public static func name(text: String) -> Attribute {
-            return .init(.new(withName: "name", text: text))
-        }
-        
-        public static func time(value: TimeInterval) -> Attribute {
-            return .init(.new(withName: "time", text: String(value)))
-        }
     }
     
     public enum TestSuite {
-        
         public typealias Element = Tagged<TestSuite, ElementNode>
         public typealias Attribute = Tagged<TestSuite, AttributeNode>
-        
-        public static func new() -> Element {
-            return .init(.new(withName: "testsuite", text: nil))
-        }
-        
-        public static func id(value: UUID) -> Attribute {
-            return .init(.new(withName: "id", text: value.uuidString))
-        }
-        
-        public static func name(text: String) -> Attribute {
-            return .init(.new(withName:"name", text: text))
-        }
-        
-        public static func tests(number: Int) -> Attribute {
-            return .init(.new(withName: "tests", text: String(number)))
-        }
-        
-        public static func failures(number: Int) -> Attribute {
-            return .init(.new(withName: "failures", text: String(number)))
-        }
-        
-        public static func time(value: TimeInterval) -> Attribute {
-            return .init(.new(withName: "time", text: String(value)))
-        }
     }
     
     public enum TestSuites {
-        
         public typealias Element = Tagged<TestSuites, ElementNode>
         public typealias Attribute = Tagged<TestSuites, AttributeNode>
-        
-        public static func new() -> Element {
-            return .init(.new(withName: "testsuites", text: nil))
-        }
-        
-        public static func id(value: UUID) -> Attribute {
-            return .init(.new(withName: "id", text: value.uuidString))
-        }
-        
-        public static func name(text: String) -> Attribute {
-            return .init(.new(withName: "name", text: text))
-        }
-        
-        public static func tests(number: Int) -> Attribute {
-            return .init(.new(withName: "tests", text: String(number)))
-        }
-        
-        public static func failures(number: Int) -> Attribute {
-            return .init(.new(withName: "failures", text: String(number)))
-        }
-        
-        public static func time(value: TimeInterval) -> Attribute {
-            return .init(.new(withName: "time", text: String(value)))
-        }
     }
 }
 
 public extension JUnit.Document {
+    
     static func new(version: String, characterEncoding: String, suites: JUnit.TestSuites.Element) -> JUnit.Document {
         let document = XMLDocument(rootElement: suites.rawValue.value)
         document.version = version
         document.characterEncoding = characterEncoding
         return .init(document)
     }
+    
+    func xmlString() -> String {
+        return self.rawValue.xmlString
+    }
 }
 
-public extension JUnit.ElementNode {
-    fileprivate static func new(withName name: String, text: String?) -> JUnit.ElementNode {
+private extension JUnit.ElementNode {
+    
+    static func new(withName name: String, text: String?) -> JUnit.ElementNode {
         let element = XMLElement(kind: .element)
         element.name = name
         element.stringValue = text
         return try! .init(element)
     }
     
-    fileprivate func set(attributes: [JUnit.AttributeNode]?) -> JUnit.ElementNode {
+    func set(attributes: [JUnit.AttributeNode]?) -> JUnit.ElementNode {
         self.value.attributes = attributes?.map { $0.value }
         return self
     }
     
-    fileprivate func set(children: [JUnit.ElementNode]?) -> JUnit.ElementNode {
+    func set(children: [JUnit.ElementNode]?) -> JUnit.ElementNode {
         self.value.setChildren(children?.map { $0.value })
         return self
     }
 }
 
-public extension JUnit.AttributeNode {
-    fileprivate static func new(withName name: String, text: String?) -> JUnit.AttributeNode {
+private extension JUnit.AttributeNode {
+    
+    static func new(withName name: String, text: String?) -> JUnit.AttributeNode {
         let attribute = XMLNode(kind: .attribute)
         attribute.name = name
         attribute.stringValue = text
@@ -179,13 +106,34 @@ public extension JUnit.AttributeNode {
 }
 
 public extension JUnit.Failure.Element {
+    
+    static func new(withText text: String) -> JUnit.Failure.Element {
+        return .init(.new(withName: "failure", text: text))
+    }
+    
     func set(attributes: [JUnit.Failure.Attribute]?) -> JUnit.Failure.Element {
         _ = self.rawValue.set(attributes: attributes?.map { $0.rawValue } )
         return self
     }
 }
 
+public extension JUnit.Failure.Attribute {
+    
+     static func message(text: String) -> JUnit.Failure.Attribute {
+        return .init(.new(withName: "message", text: text))
+    }
+    
+     static func type(text: String) -> JUnit.Failure.Attribute {
+        return .init(.new(withName: "type", text: text))
+    }
+}
+
 public extension JUnit.TestCase.Element {
+    
+    static func new() -> JUnit.TestCase.Element {
+        return .init(.new(withName: "testcase", text: nil))
+    }
+    
     func set(failures: [JUnit.Failure.Element]?) -> JUnit.TestCase.Element {
         _ = self.rawValue.set(children: failures?.map { $0.rawValue } )
         return self
@@ -197,7 +145,27 @@ public extension JUnit.TestCase.Element {
     }
 }
 
+public extension JUnit.TestCase.Attribute {
+    
+    static func id(value: UUID) -> JUnit.TestCase.Attribute  {
+        return .init(.new(withName: "id", text: value.uuidString))
+    }
+    
+    static func name(text: String) -> JUnit.TestCase.Attribute  {
+        return .init(.new(withName: "name", text: text))
+    }
+    
+    static func time(value: TimeInterval) -> JUnit.TestCase.Attribute  {
+        return .init(.new(withName: "time", text: String(value)))
+    }
+}
+
 public extension JUnit.TestSuite.Element {
+    
+    static func new() -> JUnit.TestSuite.Element {
+        return .init(.new(withName: "testsuite", text: nil))
+    }
+    
     func set(testCases: [JUnit.TestCase.Element]?) -> JUnit.TestSuite.Element {
         _ = self.rawValue.set(children: testCases?.map { $0.rawValue } )
         return self
@@ -209,7 +177,35 @@ public extension JUnit.TestSuite.Element {
     }
 }
 
+public extension JUnit.TestSuite.Attribute {
+    
+    static func id(value: UUID) -> JUnit.TestSuite.Attribute {
+        return .init(.new(withName: "id", text: value.uuidString))
+    }
+    
+    static func name(text: String) -> JUnit.TestSuite.Attribute {
+        return .init(.new(withName:"name", text: text))
+    }
+    
+    static func tests(number: Int) -> JUnit.TestSuite.Attribute {
+        return .init(.new(withName: "tests", text: String(number)))
+    }
+    
+    static func failures(number: Int) -> JUnit.TestSuite.Attribute {
+        return .init(.new(withName: "failures", text: String(number)))
+    }
+    
+    static func time(value: TimeInterval) -> JUnit.TestSuite.Attribute {
+        return .init(.new(withName: "time", text: String(value)))
+    }
+}
+
 public extension JUnit.TestSuites.Element {
+    
+    static func new() -> JUnit.TestSuites.Element {
+        return .init(.new(withName: "testsuites", text: nil))
+    }
+    
     func set(suites: [JUnit.TestSuite.Element]?) -> JUnit.TestSuites.Element {
         _ = self.rawValue.set(children: suites?.map { $0.rawValue } )
         return self
@@ -218,5 +214,28 @@ public extension JUnit.TestSuites.Element {
     func set(attributes: [JUnit.TestSuites.Attribute]?) -> JUnit.TestSuites.Element {
         _ = self.rawValue.set(attributes: attributes?.map { $0.rawValue } )
         return self
+    }
+}
+
+public extension JUnit.TestSuites.Attribute {
+    
+    static func id(value: UUID) -> JUnit.TestSuites.Attribute {
+        return .init(.new(withName: "id", text: value.uuidString))
+    }
+    
+    static func name(text: String) -> JUnit.TestSuites.Attribute {
+        return .init(.new(withName: "name", text: text))
+    }
+    
+    static func tests(number: Int) -> JUnit.TestSuites.Attribute {
+        return .init(.new(withName: "tests", text: String(number)))
+    }
+    
+    static func failures(number: Int) -> JUnit.TestSuites.Attribute {
+        return .init(.new(withName: "failures", text: String(number)))
+    }
+    
+    static func time(value: TimeInterval) -> JUnit.TestSuites.Attribute {
+        return .init(.new(withName: "time", text: String(value)))
     }
 }
