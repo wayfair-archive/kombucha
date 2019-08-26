082473e31d284c69034f2fdabf554856907b5d94
Simon-Pierre Roy <sroy1@wayfair.com>
2019-08-26 12:01:17 -0400

    order tests

--- Tests/JUnitTests/JUnitTests.swift
+++ Tests/JUnitTests/JUnitTests.swift
@@ -13,57 +13,59 @@ import XCTest
 final class JUnitTest: XCTestCase {
     
     func testCreation() {
-        let report = JUnit.Report.new().set(suites:
-            JUnit.TestSuites.Element.new().set(suites: [
-                JUnit.TestSuite.Element.new().set(testCases: [
-                    JUnit.TestCase.Element.new().set(attributes: [
-                        JUnit.TestCase.Attribute.id(value: UUID.init(uuidString: "D60AD064-2BA3-461C-A278-A8A570865281")!),
-                        JUnit.TestCase.Attribute.name(text: "1-1"),
-                        JUnit.TestCase.Attribute.time(value: 1.1)
-                        ]),
-                    JUnit.TestCase.Element.new().set(attributes: [
-                        JUnit.TestCase.Attribute.id(value: UUID.init(uuidString: "0E10E188-832C-4FAF-B404-DC35294C798B")!),
-                        JUnit.TestCase.Attribute.name(text: "1-2"),
-                        JUnit.TestCase.Attribute.time(value: 1.2)
-                        ]).set(failures: [
-                            JUnit.Failure.Element.new(withText: "A description of the failure").set(attributes: [
-                                JUnit.Failure.Attribute.message(text: "a failed test"),
-                                JUnit.Failure.Attribute.type(text: "warning")
+        let report = JUnit.Report.new()
+            .set(version: "1.0", characterEncoding: "UTF-8")
+            .set(suites:
+                JUnit.TestSuites.Element.new().set(suites: [
+                    JUnit.TestSuite.Element.new().set(testCases: [
+                        JUnit.TestCase.Element.new().set(attributes: [
+                            JUnit.TestCase.Attribute.id(value: UUID.init(uuidString: "D60AD064-2BA3-461C-A278-A8A570865281")!),
+                            JUnit.TestCase.Attribute.name(text: "1-1"),
+                            JUnit.TestCase.Attribute.time(value: 1.1)
+                            ]),
+                        JUnit.TestCase.Element.new().set(attributes: [
+                            JUnit.TestCase.Attribute.id(value: UUID.init(uuidString: "0E10E188-832C-4FAF-B404-DC35294C798B")!),
+                            JUnit.TestCase.Attribute.name(text: "1-2"),
+                            JUnit.TestCase.Attribute.time(value: 1.2)
+                            ]).set(failures: [
+                                JUnit.Failure.Element.new(withText: "A description of the failure").set(attributes: [
+                                    JUnit.Failure.Attribute.message(text: "a failed test"),
+                                    JUnit.Failure.Attribute.type(text: "warning")
+                                    ])
                                 ])
+                        ]).set(attributes: [
+                            JUnit.TestSuite.Attribute.id(value: UUID.init(uuidString: "82CB4491-ACEE-4502-9673-AFB1DD9E70F4")!),
+                            JUnit.TestSuite.Attribute.name(text: "Test name for TestSuite 1"),
+                            JUnit.TestSuite.Attribute.tests(number: 12),
+                            JUnit.TestSuite.Attribute.failures(number: 3),
+                            JUnit.TestSuite.Attribute.time(value: 10.78)
+                            ]),
+                    JUnit.TestSuite.Element.new().set(testCases: [
+                        JUnit.TestCase.Element.new().set(attributes: [
+                            JUnit.TestCase.Attribute.id(value: UUID.init(uuidString: "D60AD064-2BA3-461C-A278-A8A570865281")!),
+                            JUnit.TestCase.Attribute.name(text: "2-1"),
+                            JUnit.TestCase.Attribute.time(value: 2.1)
+                            ]),
+                        JUnit.TestCase.Element.new().set(attributes: [
+                            JUnit.TestCase.Attribute.id(value: UUID.init(uuidString: "1531DC00-832E-45D9-9B04-9893D3A93B29")!),
+                            JUnit.TestCase.Attribute.name(text: "2-1"),
+                            JUnit.TestCase.Attribute.time(value: 2.1)
                             ])
+                        ]).set(attributes: [
+                            JUnit.TestSuite.Attribute.id(value: UUID.init(uuidString: "BB495D15-CBD6-45F1-898E-61A1B917551A")!),
+                            JUnit.TestSuite.Attribute.name(text: "Test name for TestSuite 2"),
+                            JUnit.TestSuite.Attribute.tests(number: 22),
+                            JUnit.TestSuite.Attribute.failures(number: 4),
+                            JUnit.TestSuite.Attribute.time(value: 113)
+                            ]),
                     ]).set(attributes: [
-                        JUnit.TestSuite.Attribute.id(value: UUID.init(uuidString: "82CB4491-ACEE-4502-9673-AFB1DD9E70F4")!),
-                        JUnit.TestSuite.Attribute.name(text: "Test name for TestSuite 1"),
-                        JUnit.TestSuite.Attribute.tests(number: 12),
-                        JUnit.TestSuite.Attribute.failures(number: 3),
-                        JUnit.TestSuite.Attribute.time(value: 10.78)
-                        ]),
-                JUnit.TestSuite.Element.new().set(testCases: [
-                    JUnit.TestCase.Element.new().set(attributes: [
-                        JUnit.TestCase.Attribute.id(value: UUID.init(uuidString: "D60AD064-2BA3-461C-A278-A8A570865281")!),
-                        JUnit.TestCase.Attribute.name(text: "2-1"),
-                        JUnit.TestCase.Attribute.time(value: 2.1)
-                        ]),
-                    JUnit.TestCase.Element.new().set(attributes: [
-                        JUnit.TestCase.Attribute.id(value: UUID.init(uuidString: "1531DC00-832E-45D9-9B04-9893D3A93B29")!),
-                        JUnit.TestCase.Attribute.name(text: "2-1"),
-                        JUnit.TestCase.Attribute.time(value: 2.1)
+                        JUnit.TestSuites.Attribute.id(value: UUID.init(uuidString: "6C142A1F-70C6-4ACB-9968-EEB61CA6C850")!),
+                        JUnit.TestSuites.Attribute.name(text: "Test name for TestSuites"),
+                        JUnit.TestSuites.Attribute.tests(number: 34),
+                        JUnit.TestSuites.Attribute.failures(number: 23),
+                        JUnit.TestSuites.Attribute.time(value: 123.78)
                         ])
-                    ]).set(attributes: [
-                        JUnit.TestSuite.Attribute.id(value: UUID.init(uuidString: "BB495D15-CBD6-45F1-898E-61A1B917551A")!),
-                        JUnit.TestSuite.Attribute.name(text: "Test name for TestSuite 2"),
-                        JUnit.TestSuite.Attribute.tests(number: 22),
-                        JUnit.TestSuite.Attribute.failures(number: 4),
-                        JUnit.TestSuite.Attribute.time(value: 113)
-                        ]),
-                ]).set(attributes: [
-                    JUnit.TestSuites.Attribute.id(value: UUID.init(uuidString: "6C142A1F-70C6-4ACB-9968-EEB61CA6C850")!),
-                    JUnit.TestSuites.Attribute.name(text: "Test name for TestSuites"),
-                    JUnit.TestSuites.Attribute.tests(number: 34),
-                    JUnit.TestSuites.Attribute.failures(number: 23),
-                    JUnit.TestSuites.Attribute.time(value: 123.78)
-                    ])
-        ).set(version: "1.0", characterEncoding: "UTF-8")
+        )
         
         XCTAssertEqual(
             report.create() , """
